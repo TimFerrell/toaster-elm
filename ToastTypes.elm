a9f07fe5f1bc46fe1b46f4116861e40c514d77e8
James Sandridge <jims@radical-ideal.com>
2016-07-25 18:07:14 -0400

    Implement simple notion of actions: Add, Remove, Pop toasts.

--- ToastTypes.elm
+++ ToastTypes.elm
@@ -1,7 +1,10 @@
 module ToastTypes exposing (..)
 
+type alias Id = Int
+
 type alias Toast =
   { kind : String
   , title : String
   , message : String
+  , id : Id
   }
--- Toaster.elm
+++ Toaster.elm
@@ -2,6 +2,8 @@ import Html exposing (..)
 import Html.App as App
 import Html.Events exposing (..)
 
+import List exposing (..)
+
 import ToastTypes exposing (..)
 
 main : Program Never
@@ -18,26 +20,42 @@ main =
   --    , subscriptions = \_ -> Sub.none
   --    }
 
--- TODO: need to grow the model out to support something like the following:
--- AddToast Toast
--- RemoveToast Toast.id
--- (Assuming that we always immediately show toasts that we receive)
 type alias Model = List Toast
 
-fakeToast : Toast
-fakeToast = {kind = "nah", title = "greets", message = "hi there"}
+type Action
+  = NoOp -- these guys are useful with timers
+  | Add Toast
+  | Remove Id
+  | Pop
+
+fakeToast : Action
+fakeToast = Add {kind = "nah", title = "greets", message = "hi there", id = -1}
+
+popToast : Action
+popToast = Pop
 
 emptyModel : Model
 emptyModel = []
 
-update : Toast -> Model -> Model
-update toast model = toast :: model
+update : Action -> Model -> Model
+update action model =
+  case action of
+    NoOp -> model
+
+    Add toast ->
+      {toast | id = length model } :: model
+
+    Remove id ->
+      filter (\toast -> toast.id /= id) model
+
+    Pop ->
+      take ((List.length model) - 1) model
 
 init : Maybe Model -> Model
 init oldModel =
   Maybe.withDefault emptyModel oldModel
 
-renderToast : Toast -> Html Toast
+renderToast : Toast -> Html Action
 renderToast toast =
   div
     [] -- no styles for now!
@@ -46,14 +64,22 @@ renderToast toast =
         [text toast.message]
     ]
 
-view : Model -> Html Toast
+view : Model -> Html Action
 view model =
   div
     []
-    (header :: (List.map renderToast model))
+    (header :: (map renderToast model))
 
-header : Html Toast
+makeButton : Action -> String -> Html Action
+makeButton action btn_text =
+  button
+    [onClick action]
+    [text btn_text]
+
+header : Html Action
 header =
   div
     []
-    [button [onClick fakeToast] [text "Add Toast"]]
+    [ (makeButton fakeToast "Add Toast")
+    , (makeButton popToast "Remove Toast")
+    ]
